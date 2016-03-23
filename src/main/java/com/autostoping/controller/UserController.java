b87be8bba47b54ee2241ad18931e4e12243a517f
Javier Gómez <jgomez@velocitypartners.net>
2016-03-23 20:55:41 -0300

    Renamed User Controller

--- src/main/java/com/autostoping/controller/UserController.java
+++ src/main/java/com/autostoping/controller/UserController.java
@@ -18,7 +18,7 @@ import org.springframework.web.servlet.ModelAndView;
  * Handles requests for the application home page.
  */
 @Controller
-public class HomeController {
+public class UserController {
 	
 	@Autowired
 	private UserDAO userDao;
